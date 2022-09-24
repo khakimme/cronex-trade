dfcdd87725b75b1b8b05771e4fe615c9a3e4b337
khakim <iamkhakim@yandex.ru>
2022-09-24 22:54:55 +0600

    Tailwind theme add custom height value

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -5,7 +5,7 @@ import UserNavigation from "./UserNavigation";
 
 export default function Navbar() {
   return (
-    <header className="flex flex-rows justify-between px-2 py-1">
+    <header className="flex flex-rows justify-between items-center px-2 h-navbar-height">
       <div className="flex flex-rows items-center gap-5">
         <Logo />
         <Catalog />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "navbar-top-height": "38px",
+      },
+    },
   },
   plugins: [],
 };
