2096f1ace07ee696125353f05393f9cae9433ba0
khakim <iamkhakim@yandex.ru>
2022-09-25 00:43:52 +0600

    Move site description to general layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Cronex Trade</title>
+        <meta name="description" content="Cronex Trading" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,5 @@ import Head from "next/head";
 import Tools from "../components/tools";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Cronex Trade</title>
-        <meta name="description" content="Cronex Trading" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Tools />
-    </>
-  );
+  return <Tools />;
 }
