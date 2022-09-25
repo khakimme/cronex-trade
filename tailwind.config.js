6939cd5448b0803dba1b1bb67b0ed47b83df12c0
khakim <iamkhakim@yandex.ru>
2022-09-25 11:15:25 +0600

    Extend font-size style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        tiny: "0.625rem",
+      },
       height: {
         navbar: "38px",
         home: "calc(100vh - 38px)",
         table: "calc(100% - 35px)",
+      },
+      colors: {
+        "ct-blue": "#4e95f5",
+        "ct-grayish": "#313337",
+        "ct-green": "#00a159",
+        "ct-red": "#cd0124",
       },
     },
   },
